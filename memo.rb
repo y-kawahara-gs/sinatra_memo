740cdc80ce41741b3d79a96d0e583593d83bfbb9
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-09-26 13:47:41 +0900

    メモアプリの実装

--- memo.rb
+++ memo.rb
@@ -1,6 +1,82 @@
+# frozen_string_literal: true
+
 require 'sinatra'
-#require 'sinatra/roting'
+require 'sinatra/reloader'
+require 'json'
 
 get '/' do
-  'Hello world!'
+  File.open('data.json') do |file|
+    hash = JSON.load(file)
+    @hash = hash
+  end
+  erb :index
+end
+
+post '/' do
+  File.open('data.json') do |file|
+    hash = JSON.load(file)
+    @hash = hash
+  end
+  key, value = @hash.to_a.last
+  id = (key.to_i + 1).to_s
+  hash=@hash.merge(id => {'title' => params[:title], 'content' => params[:content]})
+
+  File.open('data.json', 'w') do |file|
+    str = JSON.dump(hash, file)
+  end
+  redirect to '/'
+end
+
+delete '/*' do |id|
+  File.open('data.json') do |file|
+    hash = JSON.load(file)
+    @hash = hash
+  end
+  hash = @hash
+  hash.delete(id)
+
+  File.open('data.json', 'w') do |file|
+    str = JSON.dump(hash, file)
+  end
+  redirect to '/'
+end
+
+patch '/*' do |id|
+  File.open('data.json') do |file|
+    hash = JSON.load(file)
+    @hash = hash
+  end
+  hash = @hash
+  #hash.delete(id)
+  hash[id]['title'] = params[:title]
+  hash[id]['content'] = params[:content]
+
+  File.open('data.json', 'w') do |file|
+    str = JSON.dump(hash, file)
+  end
+  redirect to '/'
+end
+
+get '/showmemo/:id' do |id|
+  @id = id
+  File.open('data.json') do |file|
+    memo = JSON.load(file)
+    @memo = memo["#{params[:id]}"]
+  end
+
+  erb :showmemo
+end
+
+get '/newmemo' do
+  erb :newmemo
+end
+
+get '/editmemo/:id' do |id|
+  @id = id
+  File.open('data.json') do |file|
+    memo = JSON.load(file)
+    @memo = memo[params[:id]]
+  end
+
+  erb :editmemo
 end
