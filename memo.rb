3f92c48a983461612bf9e585932211609f55a4a9
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-09-29 13:56:02 +0900

    JSONファイルが存在しない時の処理を追加

--- memo.rb
+++ memo.rb
@@ -3,56 +3,68 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'cgi'
 
 get '/' do
-  File.open('data.json') do |file|
-    hash = JSON.load(file)
-    @hash = hash
+  if File.exist?('data.json')
+    if File.read('data.json') == ''
+      File.open('data.json', 'w') do |file|
+        JSON.dump({}, file)
+      end
+    end
+  else
+    File.open('data.json', 'w') do |file|
+      JSON.dump({}, file)
+    end
+  end
+
+  File.open('data.json', 'r') do |file|
+    @hash = JSON.parse(file.read)
   end
   erb :index
 end
 
 post '/' do
   File.open('data.json') do |file|
-    hash = JSON.load(file)
-    @hash = hash
+    @hash = JSON.parse(file.read)
   end
-  key, value = @hash.to_a.last
-  id = (key.to_i + 1).to_s
-  hash=@hash.merge(id => {'title' => params[:title], 'content' => params[:content]})
+  if @hash == {}
+    hash = { '1' => { 'title' => params[:title], 'content' => params[:content] } }
+  else
+    key = @hash.to_a.last[0]
+    id = (key.to_i + 1).to_s
+    hash = @hash.merge(id => { 'title' => params[:title], 'content' => params[:content] })
+  end
 
   File.open('data.json', 'w') do |file|
-    str = JSON.dump(hash, file)
+    JSON.dump(hash, file)
   end
   redirect to '/'
 end
 
 delete '/*' do |id|
   File.open('data.json') do |file|
-    hash = JSON.load(file)
-    @hash = hash
+    @hash = JSON.parse(file.read)
   end
   hash = @hash
   hash.delete(id)
 
   File.open('data.json', 'w') do |file|
-    str = JSON.dump(hash, file)
+    JSON.dump(hash, file)
   end
   redirect to '/'
 end
 
 patch '/*' do |id|
   File.open('data.json') do |file|
-    hash = JSON.load(file)
-    @hash = hash
+    @hash = JSON.parse(file.read)
   end
   hash = @hash
-  #hash.delete(id)
   hash[id]['title'] = params[:title]
   hash[id]['content'] = params[:content]
 
   File.open('data.json', 'w') do |file|
-    str = JSON.dump(hash, file)
+    JSON.dump(hash, file)
   end
   redirect to '/'
 end
@@ -60,8 +72,8 @@ end
 get '/showmemo/:id' do |id|
   @id = id
   File.open('data.json') do |file|
-    memo = JSON.load(file)
-    @memo = memo["#{params[:id]}"]
+    memo = JSON.parse(file.read)
+    @memo = memo[params[:id]]
   end
 
   erb :showmemo
@@ -74,7 +86,7 @@ end
 get '/editmemo/:id' do |id|
   @id = id
   File.open('data.json') do |file|
-    memo = JSON.load(file)
+    memo = JSON.parse(file.read)
     @memo = memo[params[:id]]
   end
 
